8cddbd5540c8d3fcf8ba60873d1cb37eee0168a2
simrat39 <simrats169169@gmail.com>
2020-10-08 17:18:27 -0700

    Add example for bottom sheet

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -24,30 +24,58 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.black,
       body: Center(
-        child: RaisedButton(
-          onPressed: () => showDialog(
-            context: context,
-            builder: (context) {
-              return AlertDialog(
-                contentPadding: EdgeInsets.zero,
-                content: Container(
-                  height: MediaQuery.of(context).size.height * 0.55,
-                  width: MediaQuery.of(context).size.width * 0.35,
-                  child: SimpleColorPicker(
+        child: Column(
+          children: [
+            RaisedButton(
+              child: Text("Dialog"),
+              onPressed: () => showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    contentPadding: EdgeInsets.zero,
+                    content: Container(
+                      height: MediaQuery.of(context).size.height * 0.55,
+                      width: MediaQuery.of(context).size.width * 0.35,
+                      child: SimpleColorPicker(
+                        height: MediaQuery.of(context).size.height * 0.55,
+                        onColorSelect: (color) {
+                          print(color);
+                          Navigator.of(context).pop();
+                        },
+                        onCancel: () {
+                          Navigator.of(context).pop();
+                        },
+                        initialColor: Color(0xffe50050),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            RaisedButton(
+              child: Text("Bottom sheet"),
+              onPressed: () => showModalBottomSheet(
+                context: context,
+                builder: (context) {
+                  return Container(
                     height: MediaQuery.of(context).size.height * 0.55,
-                    onColorSelect: (color) {
-                      print(color);
-                      Navigator.of(context).pop();
-                    },
-                    onCancel: () {
-                      Navigator.of(context).pop();
-                    },
-                    initialColor: Color(0xffe50050),
-                  ),
-                ),
-              );
-            },
-          ),
+                    width: MediaQuery.of(context).size.width * 0.35,
+                    child: SimpleColorPicker(
+                      height: MediaQuery.of(context).size.height * 0.55,
+                      onColorSelect: (color) {
+                        print(color);
+                        Navigator.of(context).pop();
+                      },
+                      onCancel: () {
+                        Navigator.of(context).pop();
+                      },
+                      initialColor: Color(0xffe50050),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
